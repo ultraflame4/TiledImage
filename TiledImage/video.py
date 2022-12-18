094116dffea371845c4d2ee9e6aa727b79bfd558
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-12-18 18:08:03 +0800

    Added fancy rich progress bars for video

--- TiledImage/video.py
+++ TiledImage/video.py
@@ -1,5 +1,10 @@
 import os
 from pathlib import Path
+from typing import Union
+
+from rich.live import Live
+from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn, TimeElapsedColumn, MofNCompleteColumn
+from rich.table import Table
 
 import TiledImage
 import colorama
@@ -43,7 +48,7 @@ class Video:
 
 
 def generate_tiledimage_video(reference_video: Video, tiles: np.ndarray, tile_shape: tuple[int], useCuda=False,
-                              resize_factor: float = -1):
+                              resize_factor: float = -1, progress: Union[Progress, None] = None):
     if resize_factor < 0:
         if resize_factor == -1:
             resize_factor = 1 / max(tile_shape)
@@ -51,36 +56,42 @@ def generate_tiledimage_video(reference_video: Video, tiles: np.ndarray, tile_sh
             typer.echo(f"Invalid resize_factor: {resize_factor}")
             return
 
-    print(f"Generating tiled image from video with {reference_video.path}")
-    print(f"Video frames count: {reference_video.TotalFramesCount}")
-    print(f"Video framerate count: {reference_video.Framerate}")
-    print(f"Will resize frames by a factor of {resize_factor}")
-    print(f"Tiles shape: {tiles.shape}")
+    if progress:
+        overallProcessTask = progress.add_task(f"Processing {reference_video.path.name} Frames",
+                                               total=reference_video.TotalFramesCount)
+
+        progress.print(f"Generating tiled image from video with {reference_video.path}")
+        progress.print(f"Video frames count: {reference_video.TotalFramesCount}")
+        progress.print(f"Video framerate count: {reference_video.Framerate}")
+        progress.print(f"Will resize frames by a factor of {resize_factor}")
+        progress.print(f"Tiles shape: {tiles.shape}")
+
     os.makedirs(Path("./build/frames"), exist_ok=True)
 
     timer = ClockTimer()
     timer.start()
     index = 0
-    print("\n" * 64, end="")
+
     for frame in reference_video.getFrame():
-        print(f"{colorama.Cursor.POS(0, 0)}{colorama.ansi.clear_line()}", end="")
-        print(
-            f"{colorama.Fore.CYAN}>>>> Processing {reference_video.path} Frame [{index}/{reference_video.TotalFramesCount}] >>>>{colorama.Fore.RESET}")
+
         save_filepath = Path(f"./build/frames/{reference_video.path.name}_{index}.png")
         if save_filepath.exists():
-            print(
-                f"{colorama.Fore.YELLOW}>>>> Frame [{index}/{reference_video.TotalFramesCount}] already exits at {save_filepath}! Skipping... >>>>{colorama.Fore.RESET}")
+            if progress:
+                progress.print(f"[yellow]Skipping {save_filepath} as it already exists")
+                progress.advance(overallProcessTask, 1)
             index += 1
             continue
-        print("Resizing frame...")
+
         frame = utils.resize_image(Image.fromarray(frame), 1 / max(tile_shape))
         tiled_frame = generate_tiledimage_gu(np.asarray(frame), tiles, tile_shape, useCuda)
-        Image.fromarray(tiled_frame).save(save_filepath)
+        # Image.fromarray(tiled_frame).save(save_filepath)
         index += 1
-        print(
-            f"{colorama.Fore.GREEN}>>>>Finished and Saved Frame {index}/{reference_video.TotalFramesCount} in {timer.getTimeSinceLast()}s>>>>{colorama.Fore.RESET}")
 
-    print(f"{colorama.Fore.LIGHTGREEN_EX}>>>> Finished processing {reference_video.path} >>>>>{colorama.Fore.RESET}")
+        progress.advance(overallProcessTask, 1)
+
+    if progress:
+        progress.update(overallProcessTask, description=f"Finished processing {reference_video.path.name}",
+                        total=index)
 
 
 def video_cli(
@@ -93,13 +104,25 @@ def video_cli(
         process_type: ProcessType = typer.Option(ProcessType.guvectorize,
                                                  help="Type of processing to use. Default: guvectorize. njit IS not available for video")
 ):
+    overall_progress = Progress("{task.description}", BarColumn(), MofNCompleteColumn(),
+                                TextColumn("[progress.percentage]{task.percentage:>3.0f}%"), TimeElapsedColumn(),
+                                TimeRemainingColumn())
+    overall_progress_task = overall_progress.add_task(f"Overall Progress", total=3)
+
+    progress_table = Table.grid()
+    progress_table.add_row(overall_progress)
+
     if process_type == ProcessType.njit:
         rich.print(
             f"[red]Invalid process type[/red]: {ProcessType.njit} is not available for video processing. Please use {ProcessType.guvectorize} or {ProcessType.cuda} instead.")
         return
 
-    useCuda = process_type == ProcessType.cuda
+    with Live(progress_table, refresh_per_second=10, ):
+        useCuda = process_type == ProcessType.cuda
 
-    tiles, tile_shape = TiledImage.utils.load_imageset(Path(), "", tileset_paths)
-    video = TiledImage.video.Video(source_path)
-    TiledImage.video.generate_tiledimage_video(video, tiles, tile_shape, useCuda=useCuda, resize_factor=resize_factor)
+        tiles, tile_shape = TiledImage.utils.load_imageset(Path(), "", tileset_paths, progress=overall_progress)
+        overall_progress.advance(overall_progress_task, 1)
+        video = TiledImage.video.Video(source_path)
+        TiledImage.video.generate_tiledimage_video(video, tiles, tile_shape, useCuda=useCuda,
+                                                   resize_factor=resize_factor, progress=overall_progress)
+        overall_progress.advance(overall_progress_task, 1)
