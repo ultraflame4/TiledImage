2d7bedd5ccaed86fdbe1834300dee0a7f7ff5081
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-12-18 17:16:31 +0800

    vid cli shld actly do some work now

--- TiledImage/video.py
+++ TiledImage/video.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import TiledImage
 import colorama
 import numpy
 import numpy as np
@@ -24,11 +25,12 @@ class Video:
     @property
     def TotalFramesCount(self):
         return int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
     @property
     def Framerate(self):
         return self.cap.get(cv2.CAP_PROP_FPS)
 
-    def getFrame(self)->np.ndarray:
+    def getFrame(self) -> np.ndarray:
         while self.cap.isOpened():
             ret, frame = self.cap.read()
             if not ret:
@@ -40,32 +42,43 @@ class Video:
         self.cap.release()
 
 
-def generate_tiledimage_video(reference_video: Video, tiles: np.ndarray, tile_shape: tuple[int],useCuda=False):
+def generate_tiledimage_video(reference_video: Video, tiles: np.ndarray, tile_shape: tuple[int], useCuda=False,
+                              resize_factor: float = -1):
+    if resize_factor < 0:
+        if resize_factor == -1:
+            resize_factor = 1 / max(tile_shape)
+        else:
+            typer.echo(f"Invalid resize_factor: {resize_factor}")
+            return
 
     print(f"Generating tiled image from video with {reference_video.path}")
     print(f"Video frames count: {reference_video.TotalFramesCount}")
     print(f"Video framerate count: {reference_video.Framerate}")
+    print(f"Will resize frames by a factor of {resize_factor}")
     print(f"Tiles shape: {tiles.shape}")
-    os.makedirs(Path("./build/frames"),exist_ok=True)
+    os.makedirs(Path("./build/frames"), exist_ok=True)
 
     timer = ClockTimer()
     timer.start()
     index = 0
-    print("\n"*64,end="")
+    print("\n" * 64, end="")
     for frame in reference_video.getFrame():
-        print(f"{colorama.Cursor.POS(0,0)}{colorama.ansi.clear_line()}",end="")
-        print(f"{colorama.Fore.CYAN}>>>> Processing {reference_video.path} Frame [{index}/{reference_video.TotalFramesCount}] >>>>{colorama.Fore.RESET}")
-        save_filepath=Path(f"./build/frames/{index}.png")
+        print(f"{colorama.Cursor.POS(0, 0)}{colorama.ansi.clear_line()}", end="")
+        print(
+            f"{colorama.Fore.CYAN}>>>> Processing {reference_video.path} Frame [{index}/{reference_video.TotalFramesCount}] >>>>{colorama.Fore.RESET}")
+        save_filepath = Path(f"./build/frames/{reference_video.path.name}_{index}.png")
         if save_filepath.exists():
-            print(f"{colorama.Fore.YELLOW}>>>> Frame [{index}/{reference_video.TotalFramesCount}] already exits at {save_filepath}! Skipping... >>>>{colorama.Fore.RESET}")
-            index+=1
+            print(
+                f"{colorama.Fore.YELLOW}>>>> Frame [{index}/{reference_video.TotalFramesCount}] already exits at {save_filepath}! Skipping... >>>>{colorama.Fore.RESET}")
+            index += 1
             continue
         print("Resizing frame...")
         frame = utils.resize_image(Image.fromarray(frame), 1 / max(tile_shape))
-        tiled_frame = generate_tiledimage_gu(np.asarray(frame), tiles, tile_shape,useCuda)
+        tiled_frame = generate_tiledimage_gu(np.asarray(frame), tiles, tile_shape, useCuda)
         Image.fromarray(tiled_frame).save(save_filepath)
         index += 1
-        print(f"{colorama.Fore.GREEN}>>>>Finished and Saved Frame {index}/{reference_video.TotalFramesCount} in {timer.getTimeSinceLast()}s>>>>{colorama.Fore.RESET}")
+        print(
+            f"{colorama.Fore.GREEN}>>>>Finished and Saved Frame {index}/{reference_video.TotalFramesCount} in {timer.getTimeSinceLast()}s>>>>{colorama.Fore.RESET}")
 
     print(f"{colorama.Fore.LIGHTGREEN_EX}>>>> Finished processing {reference_video.path} >>>>>{colorama.Fore.RESET}")
 
@@ -73,14 +86,20 @@ def generate_tiledimage_video(reference_video: Video, tiles: np.ndarray, tile_sh
 def video_cli(
         source_path: Path = typer.Argument(..., help="Path to source video to use as reference"),
         save_path: Path = typer.Argument(..., help="Path to save the final result to. Eg ./out.png"),
-        tileset_paths: list[Path] = typer.Argument(..., help="Path to images used as tiles. Eg: './assets/tiles/*.png' or './assets/tiles/a.png ./assets/tiles/n.png' ..."),
-        resize_factor: float = typer.Option(-1, help="Resize factor for reference image, so that the final image is not too big. Default: -1 (resizes based on tile size)"),
-        process_type: ProcessType = typer.Option(ProcessType.guvectorize, help="Type of processing to use. Default: guvectorize. njit IS not available for video")
-              ):
-
+        tileset_paths: list[Path] = typer.Argument(...,
+                                                   help="Path to images used as tiles. Eg: './assets/tiles/*.png' or './assets/tiles/a.png ./assets/tiles/n.png' ..."),
+        resize_factor: float = typer.Option(-1,
+                                            help="Resize factor for reference image, so that the final image is not too big. Default: -1 (resizes based on tile size)"),
+        process_type: ProcessType = typer.Option(ProcessType.guvectorize,
+                                                 help="Type of processing to use. Default: guvectorize. njit IS not available for video")
+):
     if process_type == ProcessType.njit:
-        rich.print(f"[red]Invalid process type[/red]: {ProcessType.njit} is not available for video processing. Please use {ProcessType.guvectorize} or {ProcessType.cuda} instead.")
+        rich.print(
+            f"[red]Invalid process type[/red]: {ProcessType.njit} is not available for video processing. Please use {ProcessType.guvectorize} or {ProcessType.cuda} instead.")
         return
 
+    useCuda = process_type == ProcessType.cuda
 
-    pass
+    tiles, tile_shape = TiledImage.utils.load_imageset(Path(), "", tileset_paths)
+    video = TiledImage.video.Video(source_path)
+    TiledImage.video.generate_tiledimage_video(video, tiles, tile_shape, useCuda=useCuda, resize_factor=resize_factor)
